3ec79d4b6b712e7ae787e1d35f7c45442bbff0e6
patt <patt.gerr@gmail.com>
2015-05-19 09:03:30 +0200

    Removed imagemin

--- gulp/tasks/media.js
+++ gulp/tasks/media.js
@@ -15,8 +15,6 @@
 var gulp         = require('gulp'),
     svgstore     = require('gulp-svgstore'),
     svgmin       = require('gulp-svgmin'),
-    imagemin     = require('gulp-imagemin'),
-    pngquant     = require('imagemin-pngquant'),
     rename       = require('gulp-rename'),
     config       = require('../config').Media;
 
@@ -27,16 +25,6 @@ var gulp         = require('gulp'),
 ------------------------------------ */
 
 gulp.task('media', function() {
-    /*gulp
-        .src( config.images.src )
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest( config.images.dest[0] ))
-        .pipe(gulp.dest( config.images.dest[1] ));*/
-
     gulp
         .src( config.svg.src )
         .pipe(svgmin({
